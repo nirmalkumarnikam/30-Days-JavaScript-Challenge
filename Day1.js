9d291441e5d7edbef513e19703ca90663f118f7f
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-07-13 20:32:29 +0530

    Day 1 challenge complete

---
+++ Day1.js
@@ -0,0 +1,27 @@
+var Age = 21
+let name = "AndrewTate"
+const check = true
+
+let age= 21
+let city = "Mumbai"
+let check_ans = true
+const newObj = {
+    name: "RamCharan"
+}
+let myArr = [1,2,3, "Nirmal", [1,2,3,4]]
+
+console.log(typeof age);
+console.log(typeof city);
+console.log(typeof check_ans);
+console.log(typeof newObj);
+console.log(typeof myArr);
+
+let change = "initial Value"
+console.log(`this is : ${change}`);
+change = "changed Value"
+console.log(`this is : ${change}`);
+
+const val = "Cannot be changed"
+console.log(`this is value of const: ${val}`);
+// val = "this will not change"  //this will throw error
+// console.log(`this is value of const: ${val}`);
