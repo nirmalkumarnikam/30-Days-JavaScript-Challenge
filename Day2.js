eebb6f743a6a578703565ac4b16f3458a90cf7a7
Nirmalkumar Nikam <nirmalkumarnikam.official@gmail.com>
2024-07-15 12:23:53 +0530

    Day 2 done

---
+++ Day2.js
@@ -0,0 +1,24 @@
+let num1= 15
+let num2= 10
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+
+
+num1+=num2;
+console.log(num1);
+num1-=num2;
+console.log(num1);
+
+console.log(num1>num2)
+console.log(num1<num2)
+console.log(num1>=num2)
+console.log(num1<=num2)
+console.log(num1==num2)
+console.log(num1===num2)
+console.log(num1>num2 && num1 == num2)
+console.log(num1>num2 || num1 == num2)
+console.log(num1 != num2)
